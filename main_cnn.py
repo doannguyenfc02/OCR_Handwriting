5dd554573275fda37b2cee419709c8066fc4839e
doannguyenfc02 <doannguyenfc02@gmail.com>
2023-06-08 00:48:24 +0700

    doannguyenfc02

--- main_cnn.py
+++ main_cnn.py
@@ -1,49 +1,53 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.utils import to_categorical
-from PIL import Image
-from keras.models import load_model
 
+# Đọc dữ liệu từ tập train và test
+train_data = pd.read_csv('mnist_train.csv')
+test_data = pd.read_csv('mnist_test.csv')
 
-# 1. Chuẩn bị dữ liệu
-data = pd.read_csv('mnist_train.csv')
-X = data.iloc[:, 1:].values / 255.0
-y = data.iloc[:, 0].values
-X = X.reshape(-1, 28, 28, 1)
-y = to_categorical(y, num_classes=10)
+# Tách features và labels
+X_train = train_data.drop('label', axis=1).values
+y_train = train_data['label'].values
+X_test = test_data.drop('label', axis=1).values
+y_test = test_data['label'].values
 
-# 2. Tiền xử lý dữ liệu
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Tiêu chuẩn hóa giá trị pixel về khoảng [0, 1]
+X_train = X_train / 255.0
+X_test = X_test / 255.0
 
-# 3. Xây dựng mô hình CNN
+# Reshape features thành (số mẫu, chiều rộng, chiều cao, số kênh)
+X_train = X_train.reshape(-1, 28, 28, 1)
+X_test = X_test.reshape(-1, 28, 28, 1)
+
+# One-hot encoding cho labels
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# Xây dựng mô hình CNN
 model = Sequential()
+
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
-# 4. Huấn luyện mô hình
+# Compile mô hình
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Huấn luyện mô hình
 model.fit(X_train, y_train, batch_size=128, epochs=10, validation_data=(X_test, y_test))
-# 5. Đánh giá mô hình
-accuracy = model.evaluate(X_test, y_test)[1]
-print("Accuracy:", accuracy)
 
-'''
-# 6. Nhận dạng ảnh test
-image_path = 'test7.png'
-image = Image.open(image_path).convert('L')  # Chuyển đổi ảnh thành định dạng grayscale
-image = image.resize((28, 28))  # Đặt kích thước ảnh thành 28x28
-image_array = np.array(image) / 255.0  # Chuẩn hóa giá trị pixel về khoảng [0, 1]
-image_array = image_array.reshape(1, 28, 28, 1)  # Reshape ảnh thành (1, 28, 28, 1)
+# Lưu mô hình
+model.save('model_cnn_mnist.h5')
 
-# Nhận dạng ảnh
-prediction = model.predict(image_array)
-predicted_label = np.argmax(prediction)
-
-print("Predicted label:", predicted_label)
-'''
+# Đánh giá mô hình trên tập kiểm tra
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print("Độ chính xác trên tập kiểm tra: ", test_accuracy)
